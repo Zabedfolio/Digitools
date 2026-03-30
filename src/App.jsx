8cf6a2a07cbf049c77b1e3f951c149c03681e052
Zabedfolio <zabedfolio@gmail.com>
2026-03-30 14:11:46 +0600

    added the premium card section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 
 import './App.css'
+import Models from './components/Models';
+
+const getModels = async() =>{
+  const res = await fetch("/data.json")
+  return res.json();
+}
+
+const modelPromise = getModels();
 
 function App() {
 
   return (
     <>
-      
+    
+
+    <Models modelPromise={modelPromise}></Models>
+
     </>
   )
 }
